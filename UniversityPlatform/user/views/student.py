c05b2a74a307c3a6d43e7a4f74058d1da7c0fb19
Arefeh Ahmadi <arefehahmadi902@gmail.com>
2023-01-23 17:56:50 +0330

    add some views

--- UniversityPlatform/user/views/student.py
+++ UniversityPlatform/user/views/student.py
@@ -58,6 +58,7 @@ def get_student_section_view(request, student_id, term_id):
     query = '''
     SELECT *
     FROM student__section JOIN section ON student__section.section_id=section.id
+    JOIN course ON course.id=section.course_id
     WHERE student_id=%d AND section.term_id=%d;
     ''' % (
         student_id,
